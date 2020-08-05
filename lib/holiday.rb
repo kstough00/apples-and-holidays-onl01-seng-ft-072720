6c3b3669fd2f40e988dbeeab21d98aa34f0b1cf9
Kyle Stough <kstough00@gmail.com>
2020-08-05 18:40:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,8 +28,8 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
- holiday_hash[:spring].each do 
-
+ holiday_hash[:spring].each do |holiday, decorations|
+   decorations << grill
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
