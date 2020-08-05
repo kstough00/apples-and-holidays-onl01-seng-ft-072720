5e96a323890a68ca62cef00e8498746d1026914d
Kyle Stough <kstough00@gmail.com>
2020-08-05 18:47:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
 holiday_hash[:fall][:thanksgiving] = [:Columbus day]
-
+end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
